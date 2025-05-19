f3d02fc950d8df933e4d3170490cd4976bb5ea4f
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2025-05-20 00:23:59 +0500

    Add Configurations for Aggregates

---
+++ NotifyHub/NotifyHub.Infrastructure/Persistence/Configurations/SmsMessageConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NotifyHub.Domain.Aggregates;
+
+namespace NotifyHub.Infrastructure.Persistence.Configurations;
+
+public class SmsMessageConfiguration : IEntityTypeConfiguration<SmsMessage>
+{
+    public void Configure(EntityTypeBuilder<SmsMessage> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Text)
+            .HasMaxLength(500)
+            .IsRequired();
+
+        builder.HasMany(x => x.SmsSendRequests)
+               .WithOne()
+               .HasForeignKey(x => x.SmsMessageId);
+    }
+}
---
+++ NotifyHub/NotifyHub.Infrastructure/Persistence/Configurations/UserDeviceConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NotifyHub.Domain.Aggregates;
+
+namespace NotifyHub.Infrastructure.Persistence.Configurations;
+
+public class UserDeviceConfiguration : IEntityTypeConfiguration<UserDevice>
+{
+    public void Configure(EntityTypeBuilder<UserDevice> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.DeviceName)
+            .HasMaxLength(100);
+
+        builder.Property(x => x.DeviceToken)
+            .HasMaxLength(200);
+    }
+}
