8f1f6644ac0ed098eec3a8da34b9bdf8bc4d7007
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2025-05-21 00:31:19 +0500

    Create SmsController

---
+++ NotifyHub/NotifyHub/Controllers/SmsController.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using NotifyHub.Application.Features.Commands.Sms;
+
+namespace NotifyHub.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class SmsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public SmsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    /// <summary>
+    /// Sends an SMS message to the specified recipients.
+    /// </summary>
+    /// <param name="command"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    [HttpPost("send")]
+    public async Task<IActionResult> SendSms([FromBody] SendSmsCommand command, CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(command, cancellationToken);
+        return Ok(result);
+    }
+}
