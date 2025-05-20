71353fc1aef2aeeaa95c2d680e508c9611e35d5a
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2025-05-21 00:31:28 +0500

    minor update

---
+++ NotifyHub/NotifyHub.Application/Hubs/ISmsHub.cs
@@ -0,0 +1,8 @@
+﻿using NotifyHub.Domain.Aggregates;
+
+namespace NotifyHub.Application.Hubs;
+
+public interface ISmsHub
+{
+    Task DispatcherSmsAsync(UserDevice userDevice, SmsMessage smsMessage);
+}
---
+++ NotifyHub/NotifyHub.Domain/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using NotifyHub.Domain.Aggregates;
+
+namespace NotifyHub.Domain.Interfaces;
+
+public interface IApplicationDbContext
+{
+    DbSet<SmsMessage> SmsMessages { get; set; }
+    DbSet<SmsSendRequest> SmsSendRequests { get; set; }
+    DbSet<UserDevice> UserDevices { get; set; }
+
+    DatabaseFacade Database { get; }
+    int SaveChanges();
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
