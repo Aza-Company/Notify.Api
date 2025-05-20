41cc75565df485eb5bfec127a6467317054a76ac
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2025-05-21 00:31:02 +0500

    Add DependencyInjection

---
+++ NotifyHub/NotifyHub.Application/Extensions/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NotifyHub.Application.Pipeline;
+using System.Reflection;
+
+namespace NotifyHub.Application.Extensions;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection RegisterApplication(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddMediatR(Assembly.GetExecutingAssembly());
+
+        services.AddValidatorsFromAssembly(typeof(MediatR.ServiceCollectionExtensions).Assembly);
+
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+        return services;
+    }
+}
