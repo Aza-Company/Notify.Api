05e0832ffbb265a8ddba325bd77a0396e0eda8f9
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2025-05-21 00:30:00 +0500

    implement Sms using CQRS and MediatR

---
+++ NotifyHub/NotifyHub.Application/Features/Commands/Sms/SendSmsCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace NotifyHub.Application.Features.Commands.Sms;
+
+public record SendSmsCommand(
+    Guid UserId, 
+    string Text, 
+    List<string> Recipients) : IRequest<Guid>;
