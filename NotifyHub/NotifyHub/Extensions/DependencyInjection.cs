321ebe171f8a427b6a2d14d44ee1237939bc3e04
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2025-05-21 20:16:00 +0500

    Add Jwt Configurations

--- NotifyHub/NotifyHub/Extensions/DependencyInjection.cs
+++ NotifyHub/NotifyHub/Extensions/DependencyInjection.cs
@@ -1,4 +1,8 @@
-﻿using NotifyHub.Infrastructure.Extensions;
+﻿using NotifyHub.Application.Configurations;
+using NotifyHub.Infrastructure.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace NotifyHub.Api.Extensions;
 
@@ -7,7 +11,54 @@ internal static class DependencyInjection
     public static IServiceCollection RegisterApi(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddInfrastructure(configuration);
+        AddOptions(services, configuration);
+        AddAuthentication(services, configuration);
 
         return services;
+    }
+
+    public static void AddOptions(this IServiceCollection services, IConfiguration configuration)
+    {
+        services
+            .AddOptions<JwtOptions>()
+            .Bind(configuration.GetSection(JwtOptions.SectionName))
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
+    }
+
+    private static void AddAuthentication(IServiceCollection services, IConfiguration configuration)
+    {
+        var section = configuration.GetSection(JwtOptions.SectionName);
+        var jwtOptions = section.Get<JwtOptions>();
+
+        if (jwtOptions is null)
+        {
+            throw new InvalidOperationException("JWT configuration settings did not load correctly.");
+        }
+
+        services
+            .AddAuthentication(options =>
+            {
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = true;
+
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+
+                    ValidIssuer = jwtOptions.Issuer,
+                    ValidAudience = jwtOptions.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(jwtOptions.SecretKey))
+                };
+            });
     }
 }
