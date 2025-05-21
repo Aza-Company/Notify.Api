993fab2f163a5cce117ea478c7d696d615d61d17
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2025-05-21 20:11:16 +0500

    Add User table and update UserDevice

--- NotifyHub/NotifyHub.Domain/Aggregates/UserDevice.cs
+++ NotifyHub/NotifyHub.Domain/Aggregates/UserDevice.cs
@@ -1,21 +1,23 @@
 ﻿using NotifyHub.Domain.Common;
+using System.ComponentModel.DataAnnotations;
 
 namespace NotifyHub.Domain.Aggregates;
 
 public class UserDevice : AuditableEntity
 {
-    public Guid Id { get; private set; }
+    [Key]
     public Guid UserId { get; private set; }
     public string DeviceName { get; private set; }
     public string DeviceToken { get; private set; }
     public bool IsOnline { get; private set; }
     public DateTime LastSeen { get; private set; }
 
+    public User? User { get; private set; }
+
     private UserDevice() { }
 
     public UserDevice(Guid userId, string deviceName, string deviceToken)
     {
-        Id = Guid.NewGuid();
         UserId = userId;
         DeviceName = deviceName;
         DeviceToken = deviceToken;
