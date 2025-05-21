1f54a0be366b3db6cbcff8d35694378bf36eb4a7
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2025-05-21 20:15:08 +0500

    Add new Excaptions

---
+++ NotifyHub/NotifyHub.Domain/Exceptions/UnauthorizedAccessException.cs
@@ -0,0 +1,6 @@
+﻿namespace NotifyHub.Domain.Exceptions;
+
+public class UnauthorizedAccessException : ApplicationException
+{
+    public UnauthorizedAccessException(string message) : base(message) { }
+}
