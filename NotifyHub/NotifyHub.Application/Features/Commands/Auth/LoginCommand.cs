5b4a4c03589fbbce704b15d7be0174e6e6f2a660
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2025-05-21 20:13:17 +0500

    Implement Login Command and Handler

---
+++ NotifyHub/NotifyHub.Application/Features/Commands/Auth/LoginCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using NotifyHub.Application.Extensions;
+
+namespace NotifyHub.Application.Features.Commands.Auth;
+
+public record LoginCommand(
+    string Email, 
+    string Password
+    ) : IRequest<Result<LoginResponse>>;
+
+public record LoginResponse(
+    string FirstName,
+    string LastName,
+    string Token
+);
