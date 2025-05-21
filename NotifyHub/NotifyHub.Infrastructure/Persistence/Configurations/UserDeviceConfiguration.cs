5df4ee9843ca0e0c7277c98183cc5aff418b3325
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2025-05-21 20:11:39 +0500

    Add Configurations

--- NotifyHub/NotifyHub.Infrastructure/Persistence/Configurations/UserDeviceConfiguration.cs
+++ NotifyHub/NotifyHub.Infrastructure/Persistence/Configurations/UserDeviceConfiguration.cs
@@ -8,12 +8,17 @@ public class UserDeviceConfiguration : IEntityTypeConfiguration<UserDevice>
 {
     public void Configure(EntityTypeBuilder<UserDevice> builder)
     {
-        builder.HasKey(x => x.Id);
+        builder.HasKey(x => x.UserId);
 
         builder.Property(x => x.DeviceName)
             .HasMaxLength(100);
 
         builder.Property(x => x.DeviceToken)
             .HasMaxLength(200);
+
+        builder.HasOne(d => d.User)
+            .WithOne(u => u.UserDevice)
+            .HasForeignKey<UserDevice>(d => d.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
     }
 }
