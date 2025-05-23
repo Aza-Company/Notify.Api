08a0c9f591132221fb8d971e230e91aa1cc7e607
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2025-05-23 11:22:03 +0500

    update Sms Command, Handler and Controller

--- NotifyHub/NotifyHub.Application/Features/Handlers/Sms/SendSmsCommandHandler.cs
+++ NotifyHub/NotifyHub.Application/Features/Handlers/Sms/SendSmsCommandHandler.cs
@@ -1,6 +1,6 @@
 ﻿using MediatR;
-using Microsoft.EntityFrameworkCore;
 using NotifyHub.Application.Features.Commands.Sms;
+using NotifyHub.Application.Interfaces;
 using NotifyHub.Domain.Aggregates;
 using NotifyHub.Domain.Interfaces;
 
@@ -9,21 +9,27 @@ namespace NotifyHub.Application.Features.Handlers.Sms;
 internal sealed class SendSmsCommandHandler : IRequestHandler<SendSmsCommand, Guid>
 {
     private readonly IApplicationDbContext _context;
+    private readonly ICurrentUserService _currentUserService;
 
-    public SendSmsCommandHandler(IApplicationDbContext context)
+    public SendSmsCommandHandler(IApplicationDbContext context, ICurrentUserService currentUserService)
     {
-        _context = context;
+        _context = context ?? throw new ArgumentNullException(nameof(context));
+        _currentUserService = currentUserService ?? throw new ArgumentNullException(nameof(currentUserService));
     }
 
     public async Task<Guid> Handle(SendSmsCommand request, CancellationToken cancellationToken)
     {
-        var message = new SmsMessage(request.UserId, request.Text, request.Recipients);
+        ArgumentNullException.ThrowIfNull(request);
 
-        var device = await _context.UserDevices
-            .Where(d => d.UserId == request.UserId && d.IsOnline)
-            .OrderByDescending(d => d.LastSeen)
-            .FirstOrDefaultAsync(cancellationToken)
-            ?? throw new InvalidOperationException("Нет активного устройства у пользователя");
+        var userId = _currentUserService.GetUserId();
+
+        var message = new SmsMessage(userId, request.Text, request.Recipients);
+
+        //var device = await _context.UserDevices
+        //    .Where(d => d.UserId == userId && d.IsOnline)
+        //    .OrderByDescending(d => d.LastSeen)
+        //    .FirstOrDefaultAsync(cancellationToken)
+        //    ?? throw new NoActiveDeviceException("Нет активного устройства у пользователя");
 
         await _context.SmsMessages.AddAsync(message, cancellationToken);
         await _context.SaveChangesAsync(cancellationToken);
