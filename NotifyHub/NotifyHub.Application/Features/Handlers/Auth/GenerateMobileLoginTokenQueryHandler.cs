00e6be585b996e20205de12f34add90c1d4c5b13
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2025-05-23 21:30:59 +0500

    Create GenerateMobileLoginTokenQuery and Handler

---
+++ NotifyHub/NotifyHub.Application/Features/Handlers/Auth/GenerateMobileLoginTokenQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.Extensions.Caching.Distributed;
+using NotifyHub.Application.Extensions;
+using NotifyHub.Application.Features.Queries.Auth;
+using NotifyHub.Application.Interfaces;
+
+namespace NotifyHub.Application.Features.Handlers.Auth;
+
+internal sealed class GenerateMobileLoginTokenQueryHandler : IRequestHandler<GenerateMobileLoginTokenQuery, Result<string>>
+{
+    private readonly ICurrentUserService _currentUserService;
+    private readonly IDistributedCache _cache;
+
+    public GenerateMobileLoginTokenQueryHandler(ICurrentUserService currentUserService, IDistributedCache cache)
+    {
+        _currentUserService = currentUserService ?? throw new ArgumentNullException(nameof(currentUserService));
+        _cache = cache ?? throw new ArgumentNullException(nameof(cache));
+    }
+
+    public async Task<Result<string>> Handle(GenerateMobileLoginTokenQuery request, CancellationToken cancellationToken)
+    {
+        var userId = _currentUserService.GetUserId();
+        var token = Guid.NewGuid().ToString();
+        var cacheKey = $"MobileLoginToken: {token}";
+
+        await _cache.SetStringAsync(cacheKey, userId.ToString(),
+            new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(6)
+            }, cancellationToken);
+
+        return Result<string>.Success(token);
+    }
+}
---
+++ NotifyHub/NotifyHub.Application/Features/Queries/Auth/GenerateMobileLoginTokenQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using NotifyHub.Application.Extensions;
+
+namespace NotifyHub.Application.Features.Queries.Auth;
+
+public record GenerateMobileLoginTokenQuery() : IRequest<Result<string>>;
